19126da7248dd1389519cea9047a4528f326dbb6
Dhirendra Singh <dhi.singh@gmail.com>
2018-10-02 12:20:37 +1000

    Moved map to vue component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Emergency Evacuation Simulator</h1>
+    <p>&copy; 2016, 2017, 2018, RMIT University.</p>
+    <img src="../assets/collaborator-logos-row1.png"
+      class="img-responsive" alt="Collaborators">
+    <img src="../assets/collaborator-logos-row2.png"
+      class="img-responsive" alt="Collaborators">
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <mapbox
+    access-token="pk.eyJ1IjoiZGhpeHNpbmdoIiwiYSI6ImNqbWx4OTR0ZzBkMWUzb255and1aTUweGkifQ.U0vPiwyfM4ad7axC_4dkHg"
+    :map-options="{
+      style: '/styles/mapbox-dark-custom.json',
+      center: [144.218571, -37.064600], // Castlemaine VIC
+      minZoom: 0,
+      zoom: 10,
+      maxZoom: 14
+    }"
+    :geolocate-control="{
+      show: true,
+      position: 'top-left'
+    }"
+    :scale-control="{
+      show: true,
+      position: 'bottom-left'
+    }"
+    :fullscreen-control="{
+      show: true,
+      position: 'top-left'
+    }"
+  >
+  </mapbox>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Mapbox from "mapbox-gl-vue";
 export default {
   name: "home",
   components: {
-    HelloWorld
+    mapbox: Mapbox
   }
 };
 </script>
+
+<style scoped lang="css">
+#map {
+  position: relative;
+  text-align: left;
+  top: 0;
+  bottom: 0;
+  height:500px;
+  width: 100%;
+}
+</style>
