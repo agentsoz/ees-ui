9c4347fdf8b0ff426dffe6adaa1dab5a88b730be
Dhirendra Singh <dhi.singh@gmail.com>
2018-10-02 12:58:11 +1000

    Updated About page; responsive logos with bootstrap

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,8 @@
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
+  padding: 10px;
+  background-color: #eee;
   a {
     font-weight: bold;
     color: #2c3e50;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,37 @@
 <template>
   <div class="about">
     <h1>Emergency Evacuation Simulator</h1>
-    <p>&copy; 2016, 2017, 2018, RMIT University.</p>
-    <img src="../assets/collaborator-logos-row1.png"
-      class="img-responsive" alt="Collaborators">
-    <img src="../assets/collaborator-logos-row2.png"
-      class="img-responsive" alt="Collaborators">
+    <h5>&copy; 2016, 2017, 2018, RMIT University.</h5>
+    <div id="logos">
+      <b-img :src="collaborator1" fluid-grow alt="Collaborators" />
+      <b-img :src="collaborator2" fluid-grow alt="Collaborators" />
+    </div>
   </div>
 </template>
+
+<script>
+import collaborator1 from "../assets/collaborator-logos-row1.png";
+import collaborator2 from "../assets/collaborator-logos-row2.png";
+
+export default {
+  data: function() {
+    return {
+      collaborator1: collaborator1,
+      collaborator2: collaborator2
+    };
+  }
+};
+</script>
+
+<style>
+.about {
+  padding-top: 50px;
+}
+#logos {
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 50px;
+  height: auto;
+}
+</style>
