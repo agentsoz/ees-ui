70bf712c5698bcf9b6a1482a378a1028ce0b7116
Dhirendra Singh <dhi.singh@gmail.com>
2018-10-02 13:52:04 +1000

    Parse baseurl correctly for mapbox style in dev and production modes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,7 @@
 <template>
   <mapbox
     access-token="pk.eyJ1IjoiZGhpeHNpbmdoIiwiYSI6ImNqbWx4OTR0ZzBkMWUzb255and1aTUweGkifQ.U0vPiwyfM4ad7axC_4dkHg"
-    :map-options="{
-      style: '/styles/mapbox-dark-custom.json',
-      center: [144.218571, -37.064600], // Castlemaine VIC
-      minZoom: 0,
-      zoom: 10,
-      maxZoom: 14
-    }"
+    v-bind:map-options="opts"
     :geolocate-control="{
       show: true,
       position: 'top-left'
@@ -26,10 +20,25 @@
 
 <script>
 import Mapbox from "mapbox-gl-vue";
+
+var baseUrl = process.env.NODE_ENV === "production" ? "/ees-ui/" : "../../"
+var opts = {
+  style: baseUrl + "/styles/mapbox-dark-custom.json",
+  center: [144.218571, -37.0646], // Castlemaine VIC
+  minZoom: 0,
+  zoom: 10,
+  maxZoom: 14
+};
+
 export default {
   name: "home",
   components: {
     mapbox: Mapbox
+  },
+  data: function() {
+    return {
+      opts: opts
+    };
   }
 };
 </script>
