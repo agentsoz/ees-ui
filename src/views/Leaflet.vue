6f5ed3daafb136985cb32dbefdfc0ea65b73e495
luke <s3627014@student.rmit.edu.au>
2019-05-18 16:11:48 +1000

    Added leaflet map on/leaflet endpoint

---
+++ src/views/Leaflet.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+      integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+      crossorigin
+    >
+    <l-map :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-marker :lat-lng="marker"></l-marker>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import * as Vue2Leaflet from "vue2-leaflet";
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+
+export default {
+  name: "MyAwesomeMap",
+  data: function() {
+    return {
+      zoom: 13,
+      center: L.latLng(47.41322, -1.219482),
+      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: L.latLng(47.41322, -1.219482)
+    };
+  },
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  }
+};
+</script>
+
+<style>
+
+</style>
