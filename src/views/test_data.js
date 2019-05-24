2717ffedb7e9760779d2b7166527085e545b4ff8
mohamadmustafa <mohamadmu95@gmail.com>
2019-05-24 15:26:27 +1000

    create randomly moving agents around the map

---
+++ src/views/test_data.js
@@ -0,0 +1,65 @@
+export var test_data = [
+    {
+        "type": "Feature",
+        "properties": {
+          "place": {
+            "type": "unit",
+            "id": 132
+          },
+          "layer_options": {
+            "radius": 0.5,
+            "color": "red",
+            "fillColor": "red"
+          }
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.09736769811143,
+            39.904317444103114
+          ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+          "place": {
+            "type": "unit",
+            "id": 133
+          },
+          "layer_options": {
+            "radius": 0.5,
+            "color": "red",
+            "fillColor": "red"
+          }
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.09736769811143,
+            39.904317444103114
+          ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+          "place": {
+            "type": "unit",
+            "id": 134
+          },
+          "layer_options": {
+            "radius": 0.5,
+            "color": "red",
+            "fillColor": "red"
+          }
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.09736769811143,
+            39.904317444103114
+          ]
+        }
+    }
+];
