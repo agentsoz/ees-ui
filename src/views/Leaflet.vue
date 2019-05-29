279d982802ec1a11acca9ae2ec978935183df033
mohamadmustafa <mohamadmu95@gmail.com>
2019-05-29 23:13:46 +1000

    updated agents_startingPos.js file to have 500 agents from typical summer weekday.
    agent events are now streamed from server.

--- src/views/Leaflet.vue
+++ src/views/Leaflet.vue
@@ -16,10 +16,8 @@ import A from "agentmaps";
 import proj4L from "proj4leaflet";
 import proj4 from "proj4";
 
-var map_data = require("./map_data.js").map_data;
-var test_data = require("./test_data.js").test_data;
-var events_data = require("./events_data.js").events_data;
-var agent_events = require("./agent_events.js").agent_events;
+/* DO NOT LOAD THIS FILE */
+//var agent_events = require("./agent_events.js").agent_events;
 var agents_startingPos = require("./agents_startingPos.js").agents_startingPos;
 
 var bounding_box = [[39.9058, -86.091], [39.8992, -86.1017]];
@@ -28,7 +26,7 @@ let bounding_points = [[-37.804647,144.939709], [-37.822701,144.982924]];
 export default {
   data: function() {
     return {
-      totalAgentsNum: 28,
+      totalAgentsNum: 100,
       agentIndex: 0
     }
   },
@@ -88,50 +86,41 @@ export default {
       return layer;
     },
     test() {
-      var firstProjection = 'PROJCS["NAD83 / Massachusetts Mainland",GEOGCS["NAD83",DATUM["North_American_Datum_1983",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],AUTHORITY["EPSG","6269"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4269"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Lambert_Conformal_Conic_2SP"],PARAMETER["standard_parallel_1",42.68333333333333],PARAMETER["standard_parallel_2",41.71666666666667],PARAMETER["latitude_of_origin",41],PARAMETER["central_meridian",-71.5],PARAMETER["false_easting",200000],PARAMETER["false_northing",750000],AUTHORITY["EPSG","26986"],AXIS["X",EAST],AXIS["Y",NORTH]]';
-      var secondProjection = "+proj=gnom +lat_0=90 +lon_0=0 +x_0=6300000 +y_0=6300000 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
-
-      var inputProj = proj4('+proj=utm +zone=55 +south +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
-      var outputProj = proj4('EPSG:4326');
-
-      //Taken from demo tutorial
       let agentmap = L.A.agentmap(this.map);
-      // no need to for units
-      //agentmap.buildingify(bounding_points, map_data);
 
       agentmap.agentify(this.totalAgentsNum, this.seqAgentMaker);
       
-      var eventKey = Object.keys(agent_events);
-     
-     eventKey.filter(function(event){
-      agentmap.agents.eachLayer(function(agent) {
-              if(agent_events[event][agent.options.id] != null){
-                var coords = agent_events[event][agent.options.id];
-                var lat_lng = L.latLng(coords[1][1], coords[1][0]);
+      fetch(process.env.VUE_APP_EES_TILES_API + "/agents-events")
+      .then(response => response)
+      .then(body => {
+          if (body.details) {
+              // there was an error...
+              const error = body.details.map(detail => detail.message).join(". ");
+              console.log(error);
+          } else {
+              body.json().then(chunk => {
+                  var agent_events = JSON.parse(chunk);
 
-                agent.scheduleTrip(lat_lng, {type: "unanchored"}, 20);
-              }
-          });
-     });
+                  var eventKeys = Object.keys(agent_events);
      
-    
+                  eventKeys.filter(function(event) {
+                    agentmap.agents.eachLayer(function(agent) {
+                            if(agent_events[event][agent.options.id] != null) {
+                              var coords = agent_events[event][agent.options.id];
+                              var lat_lng = L.latLng(coords[1][1], coords[1][0]);
+
+                              agent.scheduleTrip(lat_lng, {type: "unanchored"}, 40);
+                            }
+                        });
+                  });
+
+              });
+          }
+      });
 
       agentmap.controller = function() {
         if (agentmap.state.ticks % 1 === 0) {
           agentmap.agents.eachLayer(function(agent) {
-
-            // if(agent_events[agentmap.state.ticks.toString() + ".0"] != null){
-            //   if(agent_events[agentmap.state.ticks.toString() + ".0"][agent.options.id] != null){
-            //     var coords = agent_events[agentmap.state.ticks.toString() + ".0"][agent.options.id];
-            //     var lat_lng = L.latLng(coords[1][1], coords[1][0]);
-
-            //     agent.scheduleTrip(lat_lng, {type: "unanchored"}, 20);
-            //   }
-            // }
-            // var random_lat_lng = L.latLng(-37.814647, 144.929708);
-
-            // agent.scheduleTrip(random_lat_lng, {type: "unanchored"}, 1);
-
             agent.moveIt();
           });
         }
