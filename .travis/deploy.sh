dd55700b84e4a8a170d5be5d5ff0e9cf2bc49cfb
jesszurc <jesszurc@gmail.com>
2019-06-05 00:14:25 +1000

    modified encrypted id_rsa files

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
+chmod 600 .travis/id_rsa.enc # Allow read access to the private key
+ssh-add .travis/id_rsa.enc # Add the private key to SSH
 
 git config --global push.default matching
 git remote add deploy ssh://travis@$IP:$PORT$DEPLOY_DIR
