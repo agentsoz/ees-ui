6845ba65fb73e05312199c5e899a99616de187c5
jesszurc <jesszurc@gmail.com>
2019-06-04 22:54:21 +1000

    created travis deployment script

---
+++ .travis/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/id_rsa # Allow read access to the private key
+ssh-add .travis/id_rsa # Add the private key to SSH
+
+git config --global push.default matching
+git remote add deploy ssh://travis@$IP:$PORT$DEPLOY_DIR
+git push deploy master
+
+# Skip this command if you don't need to execute any additional commands after deploying.
+ssh travis@$IP -p $PORT <<EOF
+  cd $DEPLOY_DIR
+  git pull
+  npm i
+EOF
