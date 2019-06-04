18f66d594a7642591dda53832d160ac90e880f96
jesszurc <jesszurc@gmail.com>
2019-06-05 00:24:11 +1000

    troubleshooting...

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,7 @@
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 id_rsa # Allow read access to the private key
 ssh-add id_rsa # Add the private key to SSH
-
+echo $(pwd)
 git config --global push.default matching
 git remote add deploy ssh://travis@$IP:$PORT$DEPLOY_DIR
 git push deploy master
@@ -11,6 +11,7 @@ git push deploy master
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh travis@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
+  echo $(pwd)
   git pull
   npm i
 EOF
